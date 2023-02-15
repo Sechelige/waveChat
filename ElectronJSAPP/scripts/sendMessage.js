0ea627aaa7ff62609d028d2aeb8ee6dbf501d6fa
grxnd3r <110067875+grxnd3r@users.noreply.github.com>
2023-02-15 21:30:01 +0100

    Added exception for empty text in inputbox

--- ElectronJSAPP/scripts/sendMessage.js
+++ ElectronJSAPP/scripts/sendMessage.js
@@ -3,20 +3,23 @@ var inputElement = document.getElementById("inputMessenger");
 
 // new event listener
 inputElement.addEventListener("keyup", function (event) {
-     
     // if enter
-     if (event.key === "Enter") {
-          var inputText = inputElement.value; // defining the inputtext
-          let htmlCode = `<div class="messageBox">
+    if (event.key === "Enter") {
+        let inputText = inputElement.value; // defining the inputtext
+
+        // if nothing in text
+        if (inputText.length > 0) {
+            let htmlCode = `<div class="messageBox">
                                         <img class="profilePictureMessage" src="images/groupChat.png">
                                         <div class="textMessage">
                                             <p class="usernameChat">Vous</p>
                                             <p class="messageContent">${inputText}</p>
                                         </div>
                                     </div>`;
-          document.getElementById("containerDiscussion").innerHTML += htmlCode;
-          document.getElementById("inputMessenger").value = "";
-          let objDiv = document.getElementById("containerDiscussion");
-          objDiv.scrollTop = objDiv.scrollHeight;
-     }
+            document.getElementById("containerDiscussion").innerHTML += htmlCode;
+            document.getElementById("inputMessenger").value = "";
+            let objDiv = document.getElementById("containerDiscussion");
+            objDiv.scrollTop = objDiv.scrollHeight;
+        }
+    }
 });
