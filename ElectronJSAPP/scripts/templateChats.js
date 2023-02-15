103f28cf147ac9e769b93dd75af8f1c2ef99df01
nino.rocafulle <nino.rocafulle@universite-paris-saclay.fr>
2023-02-15 10:59:40 +0100

    Creating a javascript directory + scripts

---
+++ ElectronJSAPP/scripts/templateChats.js
@@ -0,0 +1,20 @@
+let divs = '';
+                for (let i of Array(20).keys()) {
+                    divs += `<a href="#"> 
+                                <div class="containerChat">  
+                                <img class="groupChatImage" src="images/groupChat.png" alt="A description of the image">
+                                <div class="groupChatText"> 
+                                <p class="groupName">Nom</p>  
+                                <p class="lastMessage">Dernier message</p>  
+                                </div> 
+                                </div>
+                                </a>`;
+                }
+                document.write(divs);
+
+                const myDivs = document.getElementsByClassName("containerChat");
+                for (let i = 0; i < myDivs.length; i++) {
+                    myDivs[i].addEventListener("click", function () {
+                        this.style.backgroundColor = "red";
+                    })
+                }
