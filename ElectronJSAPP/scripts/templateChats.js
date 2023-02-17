85fd503e77b5a98008eac0b49423400e00163d27
nino.rocafulle <nino.rocafulle@universite-paris-saclay.fr>
2023-02-17 13:58:29 +0100

    Link test added for users

--- ElectronJSAPP/scripts/templateChats.js
+++ ElectronJSAPP/scripts/templateChats.js
@@ -1,34 +1,23 @@
-// Wait for the CSS file to load
-link.addEventListener('load', function() {
-  // Create the HTML elements and apply the event listeners
-  fetch('http://wavechatapi.ddns.net:6500/app/user')
-    .then((response) => response.json())
-    .then((data) => {
-      let divs = "";
-      for (let i of Array(20).keys()) {
-        divs += `<a href="#"> 
-                    <div class="containerChat">  
-                      <img class="groupChatImage" src="images/groupChat.png" alt="A description of the image">
-                      <div class="groupChatText"> 
-                        <p class="groupName">Nom</p>  
-                        <p class="lastMessage">Dernier message</p>  
-                      </div> 
-                    </div>
-                  </a>`;
-      }
-      document.write(divs);
-
-      const myDivs = document.getElementsByClassName("containerChat");
-      for (let i = 0; i < myDivs.length; i++) {
-        myDivs[i].addEventListener("click", function () {
-          this.style.backgroundColor = "#";
-        });
-      }
-      currentWindow.webContents.reload();
-      console.log(data);
-    })
-    .catch((error) => {
-      // handle any errors
-      console.error("test" + error);
-    });
-});
+// Create the HTML elements and apply the event listeners
+fetch("http://wavechatapi.ddns.net:6500/app/user")
+     .then((response) => response.json())
+     .then((data) => {
+          containerChats = document.getElementById("containerChats");
+            for (let i = 0; i < data.length; i++) {
+                containerChats.innerHTML += `<a href="#"> 
+                      <div class="containerChat">  
+                        <img class="groupChatImage" src="images/groupChat.png" alt="A description of the image">
+                        <div class="groupChatText"> 
+                          <p class="groupName">${data[i].nomUtilisateur}</p>  
+                          <p class="lastMessage">Dernier message</p>  
+                        </div> 
+                      </div>
+                    </a>`;
+            }
+          const myDivs = document.getElementsByClassName("containerChat");
+          for (let i = 0; i < myDivs.length; i++) {
+               myDivs[i].addEventListener("click", function () {
+                    this.style.backgroundColor = "#FFF";
+               });
+          }
+     });
