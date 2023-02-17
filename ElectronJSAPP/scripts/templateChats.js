06ae4a9fcdd440049e1060dfe7c7d483db0af470
nino.rocafulle <nino.rocafulle@universite-paris-saclay.fr>
2023-02-17 09:49:14 +0100

    Update script

--- ElectronJSAPP/scripts/templateChats.js
+++ ElectronJSAPP/scripts/templateChats.js
@@ -1,6 +1,9 @@
-let divs = '';
-                for (let i of Array(20).keys()) {
-                    divs += `<a href="#"> 
+// fetch("http://wavechatapi.ddns.net:6500/app/user")
+//      .then((response) => response.json())
+//      .then((data) => {
+          let divs = "";
+          for (let i of Array(20).keys()) {
+               divs += `<a href="#"> 
                                 <div class="containerChat">  
                                 <img class="groupChatImage" src="images/groupChat.png" alt="A description of the image">
                                 <div class="groupChatText"> 
@@ -9,12 +12,18 @@ let divs = '';
                                 </div> 
                                 </div>
                                 </a>`;
-                }
-                document.write(divs);
+          }
+          document.write(divs);
 
-                const myDivs = document.getElementsByClassName("containerChat");
-                for (let i = 0; i < myDivs.length; i++) {
-                    myDivs[i].addEventListener("click", function () {
-                        this.style.backgroundColor = "#";
-                    })
-                }
+          const myDivs = document.getElementsByClassName("containerChat");
+          for (let i = 0; i < myDivs.length; i++) {
+               myDivs[i].addEventListener("click", function () {
+                    this.style.backgroundColor = "#";
+               });
+          }
+          console.log(data);
+    //  })
+    //  .catch((error) => {
+    //       // handle any errors
+    //       console.error("test" + error);
+    //  });
