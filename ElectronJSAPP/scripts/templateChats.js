5c4109eab44c6356b8cfa85a5e6146bf56c91777
grxnd3r <110067875+grxnd3r@users.noreply.github.com>
2023-02-18 21:40:39 +0100

    Update

--- ElectronJSAPP/scripts/templateChats.js
+++ ElectronJSAPP/scripts/templateChats.js
@@ -5,20 +5,24 @@ fetch(`http://wavechatapi.ddns.net:6500/app/conversation/conv/user/${userId}`)
   .then((response) => response.json())
   .then((data) => {
     for (let i = 0; i < data.length; i++) {
-      containerChats.innerHTML += `<a href="#"> 
-                      <div class="containerChat">  
-                        <img class="groupChatImage" src="images/groupChat.png" alt="A description of the image">
-                        <div class="groupChatText"> 
-                          <p class="groupName">${data[i].nomConversation}</p>  
-                          <p class="lastMessage">Dernier message</p>  
-                        </div> 
-                      </div>
-                    </a>`;
+      containerChats.innerHTML += `
+        <div class="containerChat">  
+          <img class="groupChatImage" src="images/groupChat.png" alt="A description of the image">
+          <div class="groupChatText"> 
+            <p class="groupName">${data[i].nomConversation}</p>  
+            <p class="lastMessage">Dernier message</p>  
+          </div> 
+        </div>`;
     }
     const myDivs = document.getElementsByClassName("containerChat");
     for (let i = 0; i < myDivs.length; i++) {
       myDivs[i].addEventListener("click", function () {
-        this.style.backgroundColor = "#FFF";
+        // Remove background color from all divs
+        for (let j = 0; j < myDivs.length; j++) {
+          myDivs[j].classList.remove("selected");
+        }
+        // Add selected class to clicked div
+        this.classList.add("selected");
       });
     }
   });
