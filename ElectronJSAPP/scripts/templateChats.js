77e50848fd3434200a4ad10614d4344e87e0d4e3
grxnd3r <110067875+grxnd3r@users.noreply.github.com>
2023-02-18 15:21:10 +0100

    Chats on the leftbar are now real chats from the data base

--- ElectronJSAPP/scripts/templateChats.js
+++ ElectronJSAPP/scripts/templateChats.js
@@ -1,24 +1,27 @@
 // Create the HTML elements and apply the event listeners
-fetch("http://wavechatapi.ddns.net:6500/app/user")
-     .then((response) => response.json())
-     .then((data) => {
-          containerChats = document.getElementById("containerChats");
-            for (let i = 0; i < data.length; i++) {
-                containerChats.innerHTML += `<a href="#"> 
+let userId = 1;
+
+
+fetch(`http://wavechatapi.ddns.net:6500/app/conversation/conv/user/${userId}`)
+  .then((response) => response.json())
+  .then((data) => {
+    containerChats = document.getElementById("containerChats");
+    for (let i = 0; i < data.length; i++) {
+      containerChats.innerHTML += `<a href="#"> 
                       <div class="containerChat">  
                         <img class="groupChatImage" src="images/groupChat.png" alt="A description of the image">
                         <div class="groupChatText"> 
-                          <p class="groupName">${data[i].nomUtilisateur}</p>  
+                          <p class="groupName">${data[i].nomConversation}</p>  
                           <p class="lastMessage">Dernier message</p>  
                         </div> 
                       </div>
                     </a>`;
-                    console.log(data)
-            }
-          const myDivs = document.getElementsByClassName("containerChat");
-          for (let i = 0; i < myDivs.length; i++) {
-               myDivs[i].addEventListener("click", function () {
-                    this.style.backgroundColor = "#FFF";
-               });
-          }
-     });
+      console.log(data)
+    }
+    const myDivs = document.getElementsByClassName("containerChat");
+    for (let i = 0; i < myDivs.length; i++) {
+      myDivs[i].addEventListener("click", function () {
+        this.style.backgroundColor = "#FFF";
+      });
+    }
+  });
