9739c604b7c13cb2adcf99ef4100e1b1c549fc41
grxnd3r <110067875+grxnd3r@users.noreply.github.com>
2023-02-18 15:20:20 +0100

    Messages are now being sent to the server with a POST request

--- ElectronJSAPP/scripts/sendMessage.js
+++ ElectronJSAPP/scripts/sendMessage.js
@@ -1,6 +1,10 @@
 // getting message input field object
 var inputElement = document.getElementById("inputMessenger");
 
+// set idConv and idUser to your desired values
+var idConv = 1;
+var idUser = 1;
+
 // new event listener
 inputElement.addEventListener("keyup", function (event) {
     // if enter
@@ -9,17 +13,28 @@ inputElement.addEventListener("keyup", function (event) {
 
         // if nothing in text
         if (inputText.length > 0) {
-            let htmlCode = `<div class="messageBox">
-                                        <img class="profilePictureMessage" src="images/groupChat.png">
-                                        <div class="textMessage">
-                                            <p class="usernameChat">Vous</p>
-                                            <p class="messageContent">${inputText}</p>
-                                        </div>
-                                    </div>`;
-            document.getElementById("containerDiscussion").innerHTML += htmlCode;
-            document.getElementById("inputMessenger").value = "";
-            let objDiv = document.getElementById("containerDiscussion");
-            objDiv.scrollTop = objDiv.scrollHeight;
+            let currentDate = new Date().toISOString().substr(0, 10); // get current date in ISO 8601 format
+            let currentTime = new Date().toLocaleTimeString('en-US', {hour12: false}); // get current time in user's local time zone
+
+            let messageToBeSent = {
+                "contenuMessage": inputText,
+                "dateMessage": currentDate,
+                "heureMessage": currentTime
+            };
+            console.log(messageToBeSent)
+            // make POST request to server with message data
+            fetch(`http://wavechatapi.ddns.net:6500/app/message/conv/${idConv}/user/${idUser}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(messageToBeSent)
+            })
+            .then(response => response.json())
+            .then(data => console.log(data)) // handle server response
+            .catch(error => console.error(error));
+
+            document.getElementById("inputMessenger").value = ""; // clear input field
         }
     }
 });
