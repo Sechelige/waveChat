65cb9c78f8324f4542e2b1ff79aed6519b421c6e
grxnd3r <110067875+grxnd3r@users.noreply.github.com>
2023-02-19 18:20:24 +0100

    Creating exports for function scroll

--- ElectronJSAPP/scripts/containerMessagesScrolledDown.js
+++ ElectronJSAPP/scripts/containerMessagesScrolledDown.js
@@ -1,3 +1,8 @@
-
-let objDiv = document.getElementById("containerDiscussion"); // getting container object
-objDiv.scrollTop = objDiv.scrollHeight; // scrolling container to the max
+// function to scroll container to bottom
+function scrollToBottom() {
+    let objDiv = document.getElementById("containerDiscussion");
+    objDiv.scrollTop = objDiv.scrollHeight;
+  };
+  
+  // export the function
+  module.exports = scrollToBottom;
