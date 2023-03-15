5269e28a81064775403619fa297d8d6ecdc3fec8
Sechelige <toinou.deurv@gmail.com>
2023-03-15 14:54:44 +0100

    correction inscription

--- JSRestAPI/app/controllers/connexion.controller.js
+++ JSRestAPI/app/controllers/connexion.controller.js
@@ -6,7 +6,7 @@ exports.createNumcheck = (req, res) => {
 
     numCheck = Math.floor(Math.random() * 900000) + 100000;
 
-    connexion.createNumcheck(+req.params.tagUtilisateur, numCheck, (err, data) => {
+    connexion.createNumcheck(req.params.email, numCheck, (err, data) => {
         if (err) {
             res.status(500).send({
                 message: err.message || "Some error occurred while creating the connexion."
@@ -15,28 +15,29 @@ exports.createNumcheck = (req, res) => {
         else res.send(data);
     });
 
-    user.getEmailByUser(+req.params.tagUtilisateur, (err, email) => {
+    /*user.getEmailByUser(+req.params.tagUtilisateur, (err, email) => {
         if (err) {
             console.log("Erreur lors de la récupération de l'email : ", err);
             return err;
         } else {
-            console.log("Email de l'utilisateur : ", email);
-            //send numCheck to the user's email
-            var mailOptions = {
-                from: 'wavechathelp@gmail.com',
-                to: email,
-                subject: 'WaveChat - Connection à votre compte',
-                text: 'Voici votre code de vérification : ' + numCheck,
-                html: "<h2>Voici votre code de vérification : </h2><h3> " + numCheck + "</h3></br></br></br><p>Cordialement l'équipe WaveChat.</p>"
-            };
+        }
+    });*/
 
-            nodemailer.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Email sent: ' + info.response);
-                }
-            });
+    console.log("Email de l'utilisateur : ", req.params.email);
+    //send numCheck to the user's email
+    var mailOptions = {
+        from: 'wavechathelp@gmail.com',
+        to: req.params.email,
+        subject: 'WaveChat - Connection à votre compte',
+        text: 'Voici votre code de vérification : ' + numCheck,
+        html: "<h2>Voici votre code de vérification : </h2><h3> " + numCheck + "</h3></br></br></br><p>Cordialement l'équipe WaveChat.</p>"
+    };
+
+    nodemailer.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
         }
     });
 };
