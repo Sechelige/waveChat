e1ad49e51277f2c9c25b539c0814ff09b4d4116f
Elena Ferreira <elena.ferreira@universite-paris-saclay.fr>
2023-04-17 22:54:39 +0200

    ajout des pages

---
+++ downloadSite/js/header.js
@@ -0,0 +1,16 @@
+var header = document.getElementsByTagName("header")[0]
+
+header.innerHTML = 
+`<nav>
+<ul>
+    <a href="index.html"><img id="mainLogo" src="/img/icon.png" alt="Wave"></a>
+    <li><a href="index.html">Téléchargement</a></li>
+    <li><a href="about.html">L'application</a></li>
+    <li><a href="project.html">Le projet</a></li>
+    <li><a href="installation.html">Guide</a></li>
+    <li><a href="equipe.html">L'équipe</a></li>
+    <li><a href="contact.html">Contact</a></li>
+
+    <a id="downloadHeader" href="#h2content">Télécharger</a>
+</ul>
+</nav>`
