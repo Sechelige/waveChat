64b0e8f0153da170002c7cc7c64d659580bdadfc
grxnd3r <110067875+grxnd3r@users.noreply.github.com>
2023-04-18 00:11:03 +0200

    Update site

--- downloadSite/js/header.js
+++ downloadSite/js/header.js
@@ -3,7 +3,7 @@ var header = document.getElementsByTagName("header")[0]
 header.innerHTML = 
 `<nav>
 <ul>
-    <a href="index.html"><img id="mainLogo" src="/img/icon.png" alt="Wave"></a>
+    <a href="index.html"><img id="mainLogo" src="img/icon.png" alt="Wave"></a>
     <li><a href="index.html">Téléchargement</a></li>
     <li><a href="about.html">L'application</a></li>
     <li><a href="project.html">Le projet</a></li>
