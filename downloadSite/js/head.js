e113565da6abf26bc038c9fe9787297518a33f1a
Elena Ferreira <elena.ferreira@universite-paris-saclay.fr>
2023-04-18 15:04:37 +0200

    c le commit

---
+++ downloadSite/js/head.js
@@ -0,0 +1,7 @@
+var head = document.getElementsByTagName("head")[0]
+
+head.innerHTML = `<title>Wave</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="../style/style.css">
+<link rel="icon" href="../img/icon.png"></link>`
+
