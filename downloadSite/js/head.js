4d9c1ddbf31d697334357ddf10a3a5e62060251d
grxnd3r_ <nino.rocafulle@universite-paris-saclay.fr>
2023-04-18 15:33:34 +0200

    Update site

--- downloadSite/js/head.js
+++ downloadSite/js/head.js
@@ -2,6 +2,6 @@ var head = document.getElementsByTagName("head")[0]
 
 head.innerHTML = `<title>Wave</title>
 <meta charset="utf-8">
-<link rel="stylesheet" href="../style/style.css">
-<link rel="icon" href="../img/icon.png"></link>`
+<link rel="stylesheet" href="style/style.css">
+<link rel="icon" href="img/icon.png"></link>`
 
